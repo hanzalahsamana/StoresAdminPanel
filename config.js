2011aa5b41ebe3cf182010a7fc1037e32a04b3ca
hanzalahsamana <hanzalahsamana789@gmail.com>
2025-06-18 15:06:47 +0500

    okay . ro

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
 export const Base_Domain =
-  process.env.NODE_ENV === "production"
-    ? "xperiode.com"
-    : "localhost:3000";
+  process.env.NODE_ENV === "production" ? "xperiode.com" : "localhost:3000";
 export const HTTP =
   process.env.NODE_ENV === "production" ? "https://" : "http://";
 
 const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://api.hannanfabrics.com/api/v1"
+    ? // ? "https://api.hannanfabrics.com/api/v1"
+      "stores-backend-git-junaid-dev-hanzalah-samana.vercel.app"
     : "http://localhost:1234/api/v1";
 
 export default BASE_URL;
