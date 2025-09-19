97cd0ed39a8885cd566554ec765a32c2ae851f84
hanzalahsamana <hanzalahsamana789@gmail.com>
2025-09-19 16:29:08 +0500

    remove seo in main page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,19 @@
 'use client';
 
 import Button from '@/components/Actions/Button';
-import SEO from '@/components/SEO/SEO';
+// import SEO from '@/components/SEO/SEO';
 import { useRouter } from 'next/navigation';
 
 const Home = () => {
   const router = useRouter();
   return (
     <>
-      <SEO
+      {/* <SEO
         title="Designsli: The All-in-One Platform to Create Your Online Store"
         description="Start your ecommerce journey with Designsli. Easily build and manage your branded online store, showcase products, and grow your business."
         url="https://designsli.com/"
         ogImage="/favicon.io"
-      />
+      /> */}
       <div className="flex justify-between p-5 bg-gray-100 border-b">
         <h1 className="text-[30px] font-semibold">Our Logo</h1>
         <div className="flex gap-2">
